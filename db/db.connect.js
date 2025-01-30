119678fa40f6e16bbc81532834df51c522deb669
Shubhi <80258189+Shubhi1305@users.noreply.github.com>
2025-01-30 21:13:10 +0530

    Update db.connect.js

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,22 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    const uri = process.env.MONGO_URI; // Make sure this matches your Vercel environment variable
-    if (!uri) {
-      throw new Error("MongoDB URI is missing!");
-    }
-    
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+const mongoUri = process.env.MONGODB
 
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("Error connecting to Database:", error);
-    process.exit(1); // Stop the server if database connection fails
-  }
-};
+const initializeDatabase = async() => {
+    await mongoose
+    .connect(mongoUri)
+    .then(() => {
+        console.log("Connected to Database")
+    })
+    .catch((error) => console.log("Error connecting to Database.", error))
+}
 
-module.exports = connectDB;
+
+module.exports = { initializeDatabase };
